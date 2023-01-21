7c472b63eacf90357563b4a7b9ed606866645f58
gmesh <gmeshie@gmail.com>
2023-01-21 17:37:23 +0200

    Add firebase auth, sign in and up, base for db insert

--- app/src/main/java/com/example/homechef/MainActivity.java
+++ app/src/main/java/com/example/homechef/MainActivity.java
@@ -12,15 +12,19 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     NavController navController;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_navhost);
         navController = navHostFragment.getNavController();
--- app/src/main/java/com/example/homechef/SignUpActivity.java
+++ app/src/main/java/com/example/homechef/SignUpActivity.java
@@ -1,14 +1,69 @@
 package com.example.homechef;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText userNameInput, emailInput, passwordInput;
+    private Button signUpButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        userNameInput = (EditText) findViewById(R.id.userNameInput);
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUpUser();
+            }
+        });
+    }
+
+    private void signUpUser() {
+        String userName = userNameInput.getText().toString().trim();
+        String email = emailInput.getText().toString().trim();
+        String password = passwordInput.getText().toString().trim();
+        // TODO: validate inputs
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // TODO: save user data in the database
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            navToActivity();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Sign up failed, please try again" + task.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void navToActivity() {
+        Intent navToActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(navToActivityIntent);
     }
 }
