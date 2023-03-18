6f34ca4d42d60496bf70b68a8337a72ba4578954
gmesh <gmeshie@gmail.com>
2023-03-18 22:58:15 +0200

    Page skeleton

---
+++ app/src/main/java/com/example/homechef/DisplayEditRecipeFragment.java
@@ -0,0 +1,18 @@
+package com.example.homechef;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DisplayEditRecipeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_display_edit_recipe, container, false);
+    }
+}
